dd7772228aa4f269c46661cd0fee9695adb8138c
Oraelys <alanamos0001@gmail.com>
2025-06-10 09:43:33 +0100

    Update landingPage.jsx

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function LandingPage() {
     return (
-        <div className='h-screen grid w-full bg-emerald-950'>LandingPage</div>
+        <div className='h-screen grid w-full bg-emerald-950'>LandingPage
+            <Link className=' text-white' to="/burn">Dashboard</Link>;
+        </div>
     )
 }
 
