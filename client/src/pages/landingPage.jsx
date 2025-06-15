1a388731e538f36301bb8b350ee1a37c91b8a993
Oraelys <alanamos0001@gmail.com>
2025-06-15 13:42:59 +0100

    Update landingPage.jsx

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { useAccount, useWalletClient } from 'wagmi';
 import { ethers } from 'ethers';
 import axios from 'axios';
