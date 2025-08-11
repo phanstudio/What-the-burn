b0f1061514e2de0cf103bbc8d19b01ee15b23e37
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-08-11 11:07:47 +0100

    Update wagmi.js

--- client/src/utils/wagmi.js
+++ client/src/utils/wagmi.js
@@ -30,33 +30,10 @@ const hyperEVM = defineChain({
   },
 });
 
-const hyperEVMTestnet = defineChain({
-  id: 998,
-  name: 'HyperEVMTestnet',
-  iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/32196.png',
-  iconBackground: '#fff',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'HYPE',
-    symbol: 'HYPE',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.hyperliquid-testnet.xyz/evm'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'HyperEVMTestnet Explorer',
-      url: 'https://testnet.purrsec.com/',
-    },
-  },
-});
-
 export const config = getDefaultConfig({
   appName: 'What the burn?',
   projectId: '5379c9e3bf58798f600c7cd162a5120f',
-  chains: [hyperEVM, hyperEVMTestnet], // defualt wallets
+  chains: [hyperEVM], // defualt wallets
   ssr: false, // If your dApp uses server side rendering (SSR)
   wallets: [
     {
